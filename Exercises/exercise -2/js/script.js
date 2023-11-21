b7c05d5cd3f0422fefef0c4357118aff4745138a
sandamali-iresha <sadamalisadu200@gmail.com>
2023-11-21 18:36:02 +0530

    exercises2 js

---
+++ Exercises/exercise -2/js/script.js
@@ -0,0 +1,17 @@
+alert("Hello frontend developer");
+ 
+function init() {
+    document.getElementById("about").style.color ='blue'
+}
+
+function date_add(){
+    document.getElementById('demo').innerHTML = Date()
+}
+
+function turnOnLight() {
+    document.getElementById('myImage').src='img/download (2).png'
+}
+
+function turnOffLight() {
+    document.getElementById('myImage').src='img/download.png'
+}
